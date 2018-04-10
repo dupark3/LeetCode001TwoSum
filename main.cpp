b51b9c8b0c45430907d75518835ebbae87499c71
Du Park <dupark3@gmail.com>
2018-04-10 07:47:13 -0400

    Main function outlined

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> indices;
+
+        return indices;
+    }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> input = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> resulting_indices = twoSum(input, target);
+    cout << resulting_indices[0] << ", " << resulting_indices[1] << endl;
     return 0;
 }
