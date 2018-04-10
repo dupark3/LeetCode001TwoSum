50e518bee63f88c9d23f9f4298b2d22b7bd199ad
Du Park <dupark3@gmail.com>
2018-04-10 07:56:11 -0400

    Added a class object and call member function twoSum

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,6 +7,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> indices;
+
 
         return indices;
     }
@@ -15,7 +17,11 @@ int main()
 {
     vector<int> input = {2, 7, 11, 15};
     int target = 9;
-    vector<int> resulting_indices = twoSum(input, target);
+
+    Solution s;
+    vector<int> resulting_indices = s.twoSum(input, target);
+
     cout << resulting_indices[0] << ", " << resulting_indices[1] << endl;
+
     return 0;
 }
