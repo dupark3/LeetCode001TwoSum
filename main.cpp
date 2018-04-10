f54e96721f7bf219aafb04d5f6ad6f00598c6c16
Du Park <dupark3@gmail.com>
2018-04-10 08:09:39 -0400

    n^2 solution of checking every pair

--- main.cpp
+++ main.cpp
@@ -6,22 +6,31 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> indices;
+        std::vector<int> solution;
+        size_t size_nums = nums.size();
 
+        for (int i = 0; i != size_nums; ++i){
+            for (int j = i + 1; j != size_nums; ++j){
+                if (nums[i] + nums[j] == target){
+                    return solution = {i, j};
+                }
+            }
+        }
 
-        return indices;
+        return solution; // no solution
     }
 };
 
 int main()
 {
-    vector<int> input = {2, 7, 11, 15};
-    int target = 9;
+    vector<int> input = {2, 7, 11, 15, -4, 23};
+    int target = 19;
 
     Solution s;
     vector<int> resulting_indices = s.twoSum(input, target);
-
-    cout << resulting_indices[0] << ", " << resulting_indices[1] << endl;
-
+    if (resulting_indices.size() != 0)
+        cout << resulting_indices[0] << ", " << resulting_indices[1] << endl;
+    else
+        cout << "No two sum solution" << endl;
     return 0;
 }
